a32526b401a2c1d9a04f730e077a5b02dfc72b17
Steven Scott <codingismy11to7@gmail.com>
2025-11-17 23:10:20 -0500

    add base packages

--- flake.nix
+++ flake.nix
@@ -1,12 +1,14 @@
 {
   inputs = {
     stable.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-wsl.url = "github:nix-community/NixOS-WSL";
   };
 
   outputs =
     {
       stable,
+      nixpkgs-unstable,
       nixos-wsl,
       ...
     }:
@@ -15,20 +17,51 @@
         system = "aarch64-linux";
         modules = [
           nixos-wsl.nixosModules.default
-          {
-            system.stateVersion = "25.05";
-            wsl.enable = true;
+          (
+            { pkgs, ... }:
+            {
+              system.stateVersion = "25.05";
+              wsl.enable = true;
 
-            wsl.defaultUser = "steven";
-            time.timeZone = "America/New_York";
+              wsl.defaultUser = "steven";
+              time.timeZone = "America/New_York";
 
-            nix.settings.experimental-features = [
-              "nix-command"
-              "flakes"
-            ];
+              nix.settings.experimental-features = [
+                "nix-command"
+                "flakes"
+              ];
 
-            programs.nix-ld.enable = true;
-          }
+              programs.nix-ld.enable = true;
+
+              nixpkgs.overlays = [
+                (_final: prev: {
+                  unstable = import nixpkgs-unstable {
+                    inherit (prev) system;
+                    config.allowUnfree = true;
+                  };
+                })
+              ];
+
+              environment.systemPackages =
+                let
+                  stablePkgs = with pkgs; [
+                    curl
+                    fish
+                    git
+                    vim
+                    wget
+                  ];
+                  unstablePkgs = with pkgs.unstable; [
+                    lazygit
+                    neovim
+                    nil
+                    nixfmt-rfc-style
+                    zellij
+                  ];
+                in
+                stablePkgs ++ unstablePkgs;
+            }
+          )
         ];
       };
     };
