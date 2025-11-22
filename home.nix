f9d9354165c76db373eb86c9ae82603055c038c6
Steven Scott <codingismy11to7@gmail.com>
2025-11-21 22:17:32 -0500

    refactor fish into its own file

--- home.nix
+++ home.nix
@@ -1,7 +1,6 @@
 {
   config,
   inputs,
-  lib,
   pkgs,
   ...
 }:
@@ -9,7 +8,10 @@ let
   ageKeyFile = "${config.home.homeDirectory}/dotfiles/secrets/keys.txt";
 in
 {
-  imports = [ inputs.sops-nix.homeManagerModules.sops ];
+  imports = [
+    inputs.sops-nix.homeManagerModules.sops
+    ./home/fish.nix
+  ];
 
   sops = {
     age.keyFile = ageKeyFile;
@@ -82,52 +84,6 @@ in
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/nvim";
 
   programs = {
-    fish = {
-      enable = true;
-
-      interactiveShellInit = ''
-        set fish_greeting
-
-        fzf_configure_bindings --git_status= --git_log=
-
-        bind ctrl-alt-l _lazygit_log
-        bind ctrl-alt-s _lazygit_status
-
-        fastfetch
-      '';
-
-      functions = {
-        _lazygit_status = builtins.readFile ./fishFuncs/_lazygit_status.fish;
-        _lazygit_log = builtins.readFile ./fishFuncs/_lazygit_log.fish;
-      };
-
-      shellAliases = {
-        du = "dust";
-        lg = "lazygit";
-        ls = "eza";
-        vim = "nvim";
-      };
-
-      shellAbbrs = {
-        reb = "sudo nixos-rebuild switch --flake";
-      };
-
-      plugins = with pkgs.fishPlugins; [
-        {
-          inherit (bass) src;
-          name = "bass";
-        }
-        {
-          inherit (fzf-fish) src;
-          name = "fzf.fish";
-        }
-        {
-          inherit (tide) src;
-          name = "tide";
-        }
-      ];
-    };
-
     lazygit = {
       enable = true;
       package = pkgs.unstable.lazygit;
@@ -168,36 +124,9 @@ in
             }
           ];
         };
+
       };
+
     };
-  };
-
-  home.activation = {
-    configureFishTide = lib.hm.dag.entryAfter [ "writeBoundary" ] (
-      let
-        tideArgs = [
-          "tide configure"
-          "--auto"
-          "--style=Rainbow"
-          "--prompt_colors='True color'"
-          "--show_time='12-hour format'"
-          "--rainbow_prompt_separators=Slanted"
-          "--powerline_prompt_heads=Sharp"
-          "--powerline_prompt_tails=Flat"
-          "--powerline_prompt_style='Two lines, character and frame'"
-          "--prompt_connection=Dotted"
-          "--powerline_right_prompt_frame=No"
-          "--prompt_connection_andor_frame_color=Dark"
-          "--prompt_spacing=Sparse"
-          "--icons='Many icons'"
-          "--transient=Yes"
-        ];
-      in
-      ''
-        verboseEcho "Configuring Tide for Fish shell..."
-
-        run ${pkgs.fish}/bin/fish -c "${lib.concatStringsSep " " tideArgs}"
-      ''
-    );
   };
 }
---
+++ home/fish.nix
@@ -0,0 +1,81 @@
+{
+  lib,
+  pkgs,
+  ...
+}:
+{
+  programs.fish = {
+    enable = true;
+
+    interactiveShellInit = ''
+      set fish_greeting
+
+      fzf_configure_bindings --git_status= --git_log=
+
+      bind ctrl-alt-l _lazygit_log
+      bind ctrl-alt-s _lazygit_status
+
+      fastfetch
+    '';
+
+    functions = {
+      _lazygit_status = builtins.readFile ./fishFuncs/_lazygit_status.fish;
+      _lazygit_log = builtins.readFile ./fishFuncs/_lazygit_log.fish;
+    };
+
+    shellAliases = {
+      du = "dust";
+      lg = "lazygit";
+      ls = "eza";
+      vim = "nvim";
+    };
+
+    shellAbbrs = {
+      reb = "sudo nixos-rebuild switch --flake";
+    };
+
+    plugins = with pkgs.fishPlugins; [
+      {
+        inherit (bass) src;
+        name = "bass";
+      }
+      {
+        inherit (fzf-fish) src;
+        name = "fzf.fish";
+      }
+      {
+        inherit (tide) src;
+        name = "tide";
+      }
+    ];
+  };
+
+  home.activation = {
+    configureFishTide = lib.hm.dag.entryAfter [ "writeBoundary" ] (
+      let
+        tideArgs = [
+          "tide configure"
+          "--auto"
+          "--style=Rainbow"
+          "--prompt_colors='True color'"
+          "--show_time='12-hour format'"
+          "--rainbow_prompt_separators=Slanted"
+          "--powerline_prompt_heads=Sharp"
+          "--powerline_prompt_tails=Flat"
+          "--powerline_prompt_style='Two lines, character and frame'"
+          "--prompt_connection=Dotted"
+          "--powerline_right_prompt_frame=No"
+          "--prompt_connection_andor_frame_color=Dark"
+          "--prompt_spacing=Sparse"
+          "--icons='Many icons'"
+          "--transient=Yes"
+        ];
+      in
+      ''
+        verboseEcho "Configuring Tide for Fish shell..."
+
+        run ${pkgs.fish}/bin/fish -c "${lib.concatStringsSep " " tideArgs}"
+      ''
+    );
+  };
+}
