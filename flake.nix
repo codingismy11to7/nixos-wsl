08b590ec285d282c024734595e75b5352d874aed
Steven Scott <codingismy11to7@gmail.com>
2025-11-24 22:59:36 -0500

    time to start poking

--- flake.nix
+++ flake.nix
@@ -47,7 +47,7 @@
       nixpkgs-unstable = nixpkgs;
     in
     {
-      nixosConfigurations.${hostname} = nixos-rpi.lib.nixosSystemFull {
+      nixosConfigurations.${hostname} = nixos-rpi.lib.nixosSystem {
         specialArgs = {
           nixos-raspberrypi = nixos-rpi;
         };
@@ -67,9 +67,29 @@
           (
             { pkgs, ... }:
             {
+              imports = [ ./nice-looking-console.nix ];
+
               system.stateVersion = "25.05";
-              networking.hostName = hostname;
+              networking = {
+                hostName = hostname;
+
+                networkmanager = {
+                  enable = true;
+                };
+              };
               time.timeZone = "America/New_York";
+
+              services = {
+                openssh.enable = true;
+
+                udev.extraRules = ''
+                  # Ignore partitions with "Required Partition" GPT partition attribute
+                  # On our RPis this is firmware (/boot/firmware) partition
+                  ENV{ID_PART_ENTRY_SCHEME}=="gpt", \
+                    ENV{ID_PART_ENTRY_FLAGS}=="0x1", \
+                    ENV{UDISKS_IGNORE}="1"
+                '';
+              };
 
               boot = {
                 loader.raspberryPi.bootloader = "kernel";
@@ -77,7 +97,15 @@
                 tmp.useTmpfs = true;
               };
 
-              #wsl.defaultUser = username;
+              systemd.services = {
+                # Do not take down the network for too long when upgrading,
+                # This also prevents failures of services that are restarted instead of stopped.
+                # It will use `systemctl restart` rather than stopping it with `systemctl stop`
+                # followed by a delayed `systemctl start`.
+                systemd-networkd.stopIfChanged = false;
+                # Services that are only restarted might be not able to resolve when resolved is stopped before
+                systemd-resolved.stopIfChanged = false;
+              };
 
               nix = {
                 registry = {
@@ -95,7 +123,6 @@
               };
 
               programs = {
-                nix-ld.enable = true;
                 fish = {
                   enable = true;
                   useBabelfish = true;
@@ -124,10 +151,23 @@
               };
 
               users.users.${username} = {
-                shell = pkgs.fish;
+                extraGroups = [
+                  "wheel"
+                  "networkmanager"
+                  "podman"
+                  "video"
+                ];
+                initialHashedPassword = "";
+                isNormalUser = true;
                 openssh.authorizedKeys.keys = [
                   "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAxNSrwubJSzASpdG2Tb2KuqCwfv0QQHtSEySnDlxycPrtQf1LUNyItrGxBXUPf0b3lALV64DAOSoko8w2WbiwqUcQKnDAN5uOtxgO+bCprzEsyI7eIH/xUkG2p/xX3VNxaJQVnvgfesLuiJNfVdupbKFDO7xVJ2ByfViiG8EP9cBv3a62yu6bDnqyh8fXy0YTtxu6iPhQ46kt5rr2OaaKgOacokYwmJ2OW43j/GnFPpUueyRH3Zk5X7nSFbySTKUmnPjHkh4vUTcvxyEPpT1g01JOmvdVx9eCLsB0wx8Pxso1d7Nd5u+D+e7c3LWLWddw1TKwFfO5GLRHXA4fsyKSew=="
                 ];
+                shell = pkgs.fish;
+              };
+
+              security.sudo = {
+                enable = true;
+                wheelNeedsPassword = false;
               };
 
               nixpkgs.overlays = [
